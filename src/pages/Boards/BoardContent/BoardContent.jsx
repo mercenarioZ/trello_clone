983367f815d269dc259bd3ceb02925a320ec7575
mercenarioz <funnystar320@gmail.com>
2023-09-20 13:04:01 +0700

    Finish drag a card feature within a column.

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -42,9 +42,10 @@ const BoardContent = ({ board }) => {
 
     // There is only one active drag item at a time (either a column or a card)
     const [activeDragItemId, setActiveDragItemId] = React.useState(null)
-
     const [activeDragItemType, setActiveDragItemType] = React.useState(null)
     const [activeDragItemData, setActiveDragItemData] = React.useState(null)
+    const [oldColumnWhenDragStart, setOldColumnWhenDragStart] =
+        React.useState(null)
 
     React.useEffect(() => {
         setOrderedColumns(
@@ -52,6 +53,7 @@ const BoardContent = ({ board }) => {
         )
     }, [board])
 
+    // This function returns the column (id of that column) that contains the card with the given cardId
     const findColumnByCardId = (cardId) => {
         return orderedColumns.find((column) =>
             column.cards.map((card) => card._id)?.includes(cardId)
@@ -59,14 +61,21 @@ const BoardContent = ({ board }) => {
     }
 
     const handleDragStart = (event) => {
-        // console.log('handleDragStart: ', event)
         setActiveDragItemId(event?.active?.id)
+
+        // set the current column/card type
         setActiveDragItemType(
             event?.active?.data?.current?.columnId
                 ? ACTIVE_DRAG_ITEM_TYPE.CARD
                 : ACTIVE_DRAG_ITEM_TYPE.COLUMN
-        ) // set the current column/card type
-        setActiveDragItemData(event?.active?.data?.current) // set the current column/card data
+        )
+        // set the current column/card data
+        setActiveDragItemData(event?.active?.data?.current)
+
+        // Check if the active drag item is a card. Because just only when dragging a card, we do have the `columnId` in the data
+        if (event?.active?.data?.current?.columnId) {
+            setOldColumnWhenDragStart(findColumnByCardId(event?.active?.id))
+        }
     }
 
     // This function is called when the dragged item is over another item
@@ -116,15 +125,23 @@ const BoardContent = ({ board }) => {
 
                 // Why do we need to clone the previous columns? Because we don't want to mutate the state directly (React doesn't like that) so we need to clone the previous columns, make the changes and then return the new columns
                 const nextColumns = cloneDeep(previousColumns)
-                const nextActiveColumn = nextColumns.find(col => col._id === activeColumn._id)
-                const nextOverColumn = nextColumns.find(col => col._id === overColumn._id)
+                const nextActiveColumn = nextColumns.find(
+                    (col) => col._id === activeColumn._id
+                )
+                const nextOverColumn = nextColumns.find(
+                    (col) => col._id === overColumn._id
+                )
 
                 if (nextActiveColumn) {
                     // Remove the card from the active column
-                    nextActiveColumn.cards = nextActiveColumn.cards.filter(card => card._id !== activeCardId)
+                    nextActiveColumn.cards = nextActiveColumn.cards.filter(
+                        (card) => card._id !== activeCardId
+                    )
 
                     // Update the cardOrderIds array
-                    nextActiveColumn.cardOrderIds = nextActiveColumn.cards.map(card => card._id)
+                    nextActiveColumn.cardOrderIds = nextActiveColumn.cards.map(
+                        (card) => card._id
+                    )
                 }
 
                 if (nextOverColumn) {
@@ -134,7 +151,9 @@ const BoardContent = ({ board }) => {
                     nextOverColumn.cards.splice(newCardIndex, 0, activeCardData)
 
                     // Update the cardOrderIds array
-                    nextOverColumn.cardOrderIds = nextOverColumn.cards.map(card => card._id)
+                    nextOverColumn.cardOrderIds = nextOverColumn.cards.map(
+                        (card) => card._id
+                    )
                 }
 
                 return nextColumns
@@ -143,50 +162,103 @@ const BoardContent = ({ board }) => {
     }
 
     const handleDragEnd = (event) => {
-        // console.log('handleDragEnd: ', event)
-
-        if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) {
-            return
-        }
-
         const { active, over } = event
 
         // If there is no over column (dragging out of the board), return
         if (!over || !active) return
 
-        // If the over column is not the same as the active column, continue
-        if (active.id !== over.id) {
-            // console.log('Dragged over column: ', over.id)
+        // Handle the case when dragging card
+        if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) {
+            const {
+                id: activeCardId,
+                data: { current: activeCardData },
+            } = active
+            const { id: overCardId } = over
 
-            // Take the old column index (from active.id)
-            const oldColumnIndex = orderedColumns.findIndex(
-                (col) => col._id === active.id
-            )
+            // Find the column by the active card id
+            const activeColumn = findColumnByCardId(activeCardId)
+            const overColumn = findColumnByCardId(overCardId)
 
-            // Take the new column index (from over.id)
-            const newColumnIndex = orderedColumns.findIndex(
-                (col) => col._id === over.id
-            )
+            // If activeColumn or overColumn is not found, return
+            if (!activeColumn || !overColumn) return
 
-            // Use arrayMove to change the columns order
-            // Columns array after being moved
-            const dndOrderedColumns = arrayMove(
-                orderedColumns,
-                oldColumnIndex,
-                newColumnIndex
-            )
+            if (oldColumnWhenDragStart._id !== overColumn._id) {
+                // Dragging card to another column
+            } else {
+                // Dragging card within the same column
 
-            // Get the new column order ids
-            // const dndOrderedColumnIds = dndOrderedColumns.map(col => col._id)
+                // Take the old column index (from oldColumnWhenDragStart)
+                const oldCardIndex = oldColumnWhenDragStart?.cards.findIndex(
+                    (col) => col._id === activeDragItemId
+                )
 
-            // Update the state with the new column order
-            setOrderedColumns(dndOrderedColumns)
+                // Take the new column index (from overColumn)
+                const newCardIndex = overColumn?.cards.findIndex(
+                    (col) => col._id === overCardId
+                )
+
+                // Use arrayMove. Dragging a card within the same column is the same as dragging a column
+                const dndOrderedCards = arrayMove(
+                    oldColumnWhenDragStart?.cards,
+                    oldCardIndex,
+                    newCardIndex
+                )
+
+                setOrderedColumns((previousColumns) => {
+                    const nextColumns = cloneDeep(previousColumns)
+
+                    // Get the column we are dragging the card in
+                    const columnThatContainsTheCard = nextColumns.find(
+                        (col) => col._id === overColumn._id
+                    )
+
+                    // Update the cards array
+                    columnThatContainsTheCard.cards = dndOrderedCards
+                    columnThatContainsTheCard.cardOrderIds =
+                        dndOrderedCards.map((card) => card._id)
+
+                    return nextColumns
+                })
+            }
         }
 
-        // Reset the active drag item
+        // Handle the case when dragging column
+        if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) {
+            // If the over column is not the same as the active column, continue
+            if (active.id !== over.id) {
+                // console.log('Dragged over column: ', over.id)
+
+                // Take the old column index (from active.id)
+                const oldColumnIndex = orderedColumns.findIndex(
+                    (col) => col._id === active.id
+                )
+
+                // Take the new column index (from over.id)
+                const newColumnIndex = orderedColumns.findIndex(
+                    (col) => col._id === over.id
+                )
+
+                // Use arrayMove to change the columns order
+                // Columns array after being moved
+                const dndOrderedColumns = arrayMove(
+                    orderedColumns,
+                    oldColumnIndex,
+                    newColumnIndex
+                )
+
+                // Get the new column order ids
+                // const dndOrderedColumnIds = dndOrderedColumns.map(col => col._id)
+
+                // Update the state with the new column order
+                setOrderedColumns(dndOrderedColumns)
+            }
+        }
+
+        // Reset the active drag item, but why? Because we don't want to keep the active drag item in the state after the drag is done
         setActiveDragItemType(null)
         setActiveDragItemId(null)
         setActiveDragItemData(null)
+        setOldColumnWhenDragStart(null)
     }
 
     const dropAnimation = {
